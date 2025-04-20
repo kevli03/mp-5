9b626cf8b39ab6c9fa21b5fd266fb4cc4e0451fc
Kevin <kevinliz2003@gmail.com>
2025-04-19 23:50:31 -0400

    not found fix

--- lib/isAliasTaken.ts
+++ lib/isAliasTaken.ts
@@ -3,5 +3,14 @@ import getCollection, {LINK_COLLECTION } from "@/db";
 export default async function isAliasTaken(alias: string): Promise<boolean> {
     const postsCollection = await getCollection(LINK_COLLECTION);
     const data = postsCollection.find({ alias: alias });
-    return data !== null;
+
+    if (data === null) {
+        return false;
+    }
+
+    data.map(() => {
+        return true;
+    });
+
+    return false;
 }
